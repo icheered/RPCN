a6eb87d27bba3f322c58af9903b715e1c86a8b31
Karina Skurule <metakarinaskurule@gmail.com>
2022-12-19 14:47:08 +0100

    lmao

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import rosbag
-
+bag = rosbag.Bag('data/run1.bag')
+bagselect2 = select(bag,'Time',...
+    [bagselect.StartTime bagselect.StartTime + 1],'Topic','/odom');
 try:
-    bag = rosbag.Bag('data/run1.bag')
-
     print(f"Message count: {bag.get_message_count()}")
     print(f"Start time: {bag.get_start_time()}")
     print(f"End time: {bag.get_end_time()}")
@@ -11,11 +11,12 @@ try:
     topics = list(bag.get_type_and_topic_info()[1].keys())
     print("Topics: ")
     for topic in topics:
-        print(f"  - {topic}")
+        if topic == '/imu/acceleration':
+            print(bag.)
 
 
-    for topic, msg, t in bag.read_messages(topics=['/imu/acceleration']):
-        print(f"Topic: {topic}, Time: {t}, Message: {msg}")
+    # for topic, msg, t in bag.read_messages(topics=['/imu/acceleration']):
+    #     print(f"Topic: {topic}, Time: {t}, Message: {msg}")
 
 except:
     print("An exception occurred")
